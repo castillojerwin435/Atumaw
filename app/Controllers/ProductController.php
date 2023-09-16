53d19175e3b175594bcb70210a3ad93065734b81
castillojerwin435 <castillojerwin34@gmail.com>
2023-09-17 07:17:29 +0800

    view

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,6 +6,14 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+        echo $product;
+    }
+    public function castillojerwin435()
+    {
+        echo 'Cutie pie';
+    }
     public function index()
     {
         //
