3064db3d38be950f073005d2f9a51178ec8b8129
castillojerwin435 <castillojerwin34@gmail.com>
2023-09-17 01:47:03 +0800

    unknown

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,9 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+    public function castillojerwin435()
+    {
+        echo (' hello cutieeee');
+    }
+   
 }
